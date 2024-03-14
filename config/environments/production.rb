e3dbe937b3e7ea401ca4c927d593023113337a4c
Lucas <thegreatberenger@gmail.com>
2024-03-13 22:45:35 -0300

    create login functionality and its views

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,4 +94,10 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  
+  # to-do: add horuku host 
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :friends
   
   get "up" => "rails/health#show", as: :rails_health_check
